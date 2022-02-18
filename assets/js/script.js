64d49e5012f6466d7a4a2abee03d98e58346b6c0
zabiremu <zabirraihan@gmail.com>
2022-02-18 13:43:33 +0600

    Panda ecommerce header, banner, catagories section complete

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('.banner-details-area').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+      });
+})
